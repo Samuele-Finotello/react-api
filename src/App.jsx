6e17c6271ca7b607fd4f40c0698e8842764f1ef4
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-09-07 00:04:01 +0200

    [add] feat chiamata ajax

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useState, useEffect } from "react"
 import axios from "axios"
 
 const App = () => {
-  return (
-    <div>
 
+  const [actresses, setActresses] = useState([])
+
+  const carica = () => {
+    axios.get('https://lanciweb.github.io/demo/api/actresses').then((resp) => {
+      console.log(resp.data)
+      setActresses(resp.data)
+    })
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <div className="card mt-5">
+            <div className="card-body">
+              <div className="card-title"></div>
+              <div className="card-content"></div>
+            </div>
+          </div>
+          <button className="btn btn-primary" onClick={() => { carica() }}>Carica</button>
+        </div>
+      </div>
     </div>
   )
 }
